5bc1177d6461a26aad8da7ba0b2985c2de434d99
Steven <steven_1995@ovi.com>
2014-06-27 04:00:02 +0700

    paper mau apalagi? wa ngak kasih nampak banyak

--- Snake Multiplayer1/Snake Multiplayer/Form2.Designer.cs
+++ Snake Multiplayer1/Snake Multiplayer/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,7 +38,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 20);
             this.label1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(16, 172);
+            this.button1.Location = new System.Drawing.Point(12, 176);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 30);
             this.button1.TabIndex = 2;
@@ -59,13 +60,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 47);
+            this.label3.Location = new System.Drawing.Point(3, 29);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(248, 112);
+            this.label3.Size = new System.Drawing.Size(259, 144);
             this.label3.TabIndex = 3;
-            this.label3.Text = "- Use direction button to move the snake \r\nand eat the food for the player 1 and " +
-    "\r\nWASD for Player 2\r\n\r\n- Don\'t eat either your own body or your \r\nopponent body " +
-    "or the game will end \r\nimmidiately";
+            this.label3.Text = resources.GetString("label3.Text");
             // 
             // Form2
             // 
